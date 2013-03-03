946cd7d0ca4494e91528c99158c31aac5afb4756
Roger Allen <rallen@gmail.com>
2013-03-03 10:03:08 -0800

    Basic idea seems to work.  Bring up a window & start playing fragment shader.  Need to figure out gl_FragColor wackiness.

--- src/shadertone/shader.clj
+++ src/shadertone/shader.clj
@@ -6,7 +6,6 @@
            (org.lwjgl.util.glu GLU)))
 
 ;; ======================================================================
-;; start with spinning triangle
 (defn init-window
   [width height title]
   (let [pixel-format (PixelFormat.)
@@ -17,43 +16,38 @@
     (def globals (ref {:width width
                        :height height
                        :title title
-                       :angle 0.0
+                       :start-time current-time-millis
                        :last-time current-time-millis
                        ;; geom ids
                        :vao-id 0
                        :vbo-id 0
-                       :vboc-id 0
                        :vboi-id 0
                        :indices-count 0
                        ;; shader program ids
                        :vs-id 0
                        :fs-id 0
                        :p-id 0
-                       ::angle-loc 0}))
+                       ;; shader program uniform
+                       :i-resolution-loc 0
+                       :i-global-time-loc 0}))
     (Display/setDisplayMode (DisplayMode. width height))
     (Display/setTitle title)
     (Display/create pixel-format context-attributes)))
 
 (defn init-buffers
   []
-  ;; FIXME – DRY!
   (let [vertices (float-array
-                  [0.500  0.000 0.0 1.0
-                   -0.25  0.433 0.0 1.0
-                   -0.25 -0.433 0.0 1.0])
+                  [-1.0 -1.0 0.0 1.0
+                    1.0 -1.0 0.0 1.0
+                   -1.0  1.0 0.0 1.0
+                    1.0 -1.0 0.0 1.0
+                    1.0  1.0 0.0 1.0])
         vertices-buffer (-> (BufferUtils/createFloatBuffer (count vertices))
                             (.put vertices)
                             (.flip))
-        colors (float-array
-                [1.0 0.0 0.0
-                 0.0 1.0 0.0
-                 0.0 0.0 1.0])
-        colors-buffer (-> (BufferUtils/createFloatBuffer (count colors))
-                          (.put colors)
-                          (.flip))
         indices (byte-array
                  (map byte
-                      [0 1 2])) ;; otherwise it whines about longs
+                      [0 1 2  2 3 4]))
         indices-count (count indices)
         indices-buffer (-> (BufferUtils/createByteBuffer indices-count)
                            (.put indices)
@@ -66,12 +60,6 @@
         _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo-id)
         _ (GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices-buffer GL15/GL_STATIC_DRAW)
         _ (GL20/glVertexAttribPointer 0 4 GL11/GL_FLOAT false 0 0)
-        _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
-        ;; create & bind VBO for colors
-        vboc-id (GL15/glGenBuffers)
-        _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vboc-id)
-        _ (GL15/glBufferData GL15/GL_ARRAY_BUFFER colors-buffer GL15/GL_STATIC_DRAW)
-        _ (GL20/glVertexAttribPointer 1 3 GL11/GL_FLOAT false 0 0)
         _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
         ;; deselect the VAO
         _ (GL30/glBindVertexArray 0)
@@ -86,7 +74,6 @@
                      (assoc @globals
                        :vao-id vao-id
                        :vbo-id vbo-id
-                       :vboc-id vboc-id
                        :vboi-id vboi-id
                        :indices-count indices-count)))))
 
@@ -94,32 +81,27 @@
   (str "#version 150 core\n"
        "\n"
        "in vec4 in_Position;\n"
-       "in vec4 in_Color;\n"
-       "uniform float in_Angle;\n"
-       "\n"
-       "out vec4 pass_Color;\n"
        "\n"
        "void main(void) {\n"
-       "    float angle = in_Angle*(3.1415926535/180);\n"
-       "    mat4x4 mvp = mat4x4(0.0);\n"
-       "    mvp[0] = vec4( cos(angle), sin(angle), 0.0, 0.0);\n"
-       "    mvp[1] = vec4(-sin(angle), cos(angle), 0.0, 0.0);\n"
-       "    mvp[2] = vec4(0.0, 0.0, 1.0, 0.0);\n"
-       "    mvp[3] = vec4(0.0, 0.0, 0.0, 1.0);\n"
-       "    gl_Position = mvp*in_Position;\n"
-       "    pass_Color = in_Color;\n"
+       "    gl_Position = in_Position;\n"
        "}\n"
        ))
 
 (def fs-shader
   (str "#version 150 core\n"
        "\n"
-       "in vec4 pass_Color;\n"
-       "\n"
-       "out vec4 out_Color;\n"
+       "uniform vec3      iResolution;\n"
+       "uniform float     iGlobalTime;\n" 
+       ;;"uniform float     iChannelTime[4];\n"
+       ;;"uniform vec4      iMouse;\n"
+       ;;"uniform sampler2D iChannel[4];\n"
+       ;;"uniform vec4      iDate;\n"
+       "out vec4 o_FragColor;\n"
        "\n"
        "void main(void) {\n"
-       "    out_Color = pass_Color;\n"
+       "  vec2 vUV = (gl_FragCoord.xy / iResolution.xy);\n"
+       "  vec4 c = vec4(vUV, sin(iGlobalTime), 1.0);\n"
+       "  o_FragColor = c;\n"
        "}\n"
        ))
                      
@@ -127,9 +109,11 @@
   [shader-str shader-type]
   (let [shader-id (GL20/glCreateShader shader-type)
         _ (GL20/glShaderSource shader-id shader-str)
-        ;;_ (println "init-shaders glShaderSource errors?" (GL11/glGetError))
         _ (GL20/glCompileShader shader-id)
-        ;;_ (println "init-shaders glCompileShader errors?" (GL11/glGetError))
+        gl-compile-status (GL20/glGetShaderi shader-id GL20/GL_COMPILE_STATUS)
+        _ (when (== gl-compile-status GL11/GL_FALSE)
+            (println "ERROR: Loading a Shader:")
+            (println (GL20/glGetShaderInfoLog shader-id 10000)))
         ]
     shader-id))
 
@@ -141,15 +125,22 @@
         _ (GL20/glAttachShader p-id vs-id)
         _ (GL20/glAttachShader p-id fs-id)
         _ (GL20/glLinkProgram p-id)
-        angle-loc (GL20/glGetUniformLocation p-id "in_Angle")
-        ;;_ (println "init-shaders errors?" (GL11/glGetError))
+        gl-link-status (GL20/glGetShaderi p-id GL20/GL_LINK_STATUS)
+        _ (when (== gl-link-status GL11/GL_FALSE)
+            (println "ERROR: Linking Shaders:")
+            (println (GL20/glGetProgramInfoLog p-id 10000)))
+        i-resolution-loc (GL20/glGetUniformLocation p-id "iResolution")
+        i-global-time-loc (GL20/glGetUniformLocation p-id "iGlobalTime")
+        ;; FIXME add rest of uniforms
         ]
     (dosync (ref-set globals
                      (assoc @globals
                        :vs-id vs-id
                        :fs-id fs-id
                        :p-id p-id
-                       :angle-loc angle-loc)))))
+                       :i-resolution-loc i-resolution-loc
+                       :i-global-time-loc i-global-time-loc 
+                       )))))
 
 (defn init-gl
   []
@@ -166,21 +157,24 @@
 
 (defn draw
   []
-  (let [{:keys [width height angle angle-loc
+  (let [{:keys [width height i-resolution-loc
+                start-time last-time i-global-time-loc
                 p-id vao-id vboi-id
                 indices-count]} @globals
                 w2 (/ width 2.0)
-                h2 (/ height 2.0)]
+                h2 (/ height 2.0)
+                cur-time (/ (- last-time start-time) 1000.0)]
+    
     (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
 
     (GL20/glUseProgram p-id)
     ;; setup our uniform
-    (GL20/glUniform1f angle-loc angle)
+    (GL20/glUniform3f i-resolution-loc width height 0) ;; FIXME what is 3rd iResolution param
+    (GL20/glUniform1f i-global-time-loc cur-time)
     ;; Bind to the VAO that has all the information about the
     ;; vertices
     (GL30/glBindVertexArray vao-id)
     (GL20/glEnableVertexAttribArray 0)
-    (GL20/glEnableVertexAttribArray 1)
     ;; Bind to the index VBO that has all the information about the
     ;; order of the vertices
     (GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER vboi-id)
@@ -189,7 +183,6 @@
     ;; Put everything back to default (deselect)
     (GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER 0)
     (GL20/glDisableVertexAttribArray 0)
-    (GL20/glDisableVertexAttribArray 1)
     (GL30/glBindVertexArray 0)
     (GL20/glUseProgram 0)
     ;;(println "draw errors?" (GL11/glGetError))
@@ -197,22 +190,14 @@
 
 (defn update
   []
-  (let [{:keys [width height angle last-time]} @globals
-        cur-time (System/currentTimeMillis)
-        delta-time (- cur-time last-time)
-        next-angle (+ (* delta-time 0.05) angle)
-        next-angle (if (>= next-angle 360.0)
-                     (- next-angle 360.0)
-                     next-angle)]
-    (dosync (ref-set globals
-                     (assoc @globals
-                       :angle next-angle
-                       :last-time cur-time)))
+  (let [{:keys [width height last-time]} @globals
+        cur-time (System/currentTimeMillis)]
+    (dosync (ref-set globals (assoc @globals :last-time cur-time)))
     (draw)))
 
 (defn destroy-gl
   []
-  (let [{:keys [p-id vs-id fs-id vao-id vbo-id vboc-id vboi-id]} @globals]
+  (let [{:keys [p-id vs-id fs-id vao-id vbo-id vboi-id]} @globals]
     ;; Delete the shaders
     (GL20/glUseProgram 0)
     (GL20/glDetachShader p-id vs-id)
@@ -233,10 +218,6 @@
     (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
     (GL15/glDeleteBuffers vbo-id)
  
-    ;; Delete the color VBO
-    (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
-    (GL15/glDeleteBuffers vboc-id)
- 
     ;; Delete the index VBO
     (GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER 0)
     (GL15/glDeleteBuffers vboi-id)
@@ -248,7 +229,7 @@
 
 (defn run
   []
-  (init-window 800 600 "beta")
+  (init-window 800 600 "shadertone")
   (init-gl)
   (Thread/sleep 2000)
   (while (not (Display/isCloseRequested))
@@ -258,6 +239,7 @@
   (destroy-gl)
   (Display/destroy))
 
-(defn main []
-  (println "Run shader")
+(defn start-run-thread []
   (.start (Thread. run)))
+
+;; (start-run-thread)
