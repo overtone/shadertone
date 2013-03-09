fc19a970fc9e8d574df71a1c1e0327958a8c4056
Roger Allen <rallen@gmail.com>
2013-03-09 13:49:50 -0800

    Cleanup code after GL2.1 change.  Also cleanup shader removal to hopefully not "leak" shaders. Fixes #4

--- src/shadertone/shader.clj
+++ src/shadertone/shader.clj
@@ -1,5 +1,5 @@
 (ns shadertone.shader
-  (:require [clojure.pprint :as pprint]
+  (:require ;;[clojure.pprint :as pprint]
             [shadertone.voltap :as voltap]
             [watchtower.core :as watcher])
   (:import (java.nio ByteBuffer FloatBuffer)
@@ -10,7 +10,7 @@
            (org.lwjgl.util.glu GLU)))
 
 ;; ======================================================================
-(def globals (ref {:active :no}))
+(def globals (ref {:active :no}))   ;; States are: :yes, :stop, :no
 
 ;; ======================================================================
 (defn init-window
@@ -18,17 +18,15 @@
   (let [pixel-format (PixelFormat.)
         context-attributes (-> (ContextAttribs. 2 1))
         current-time-millis (System/currentTimeMillis)]
-    (def globals (ref {:active :yes  ;; :yes / :stop / :no
+    (def globals (ref {:active :yes
                        :width width
                        :height height
                        :title title
                        :start-time current-time-millis
                        :last-time current-time-millis
                        ;; geom ids
-                       :vao-id 0
                        :vbo-id 0
-                       :vboi-id 0
-                       :indices-count 0
+                       :vertices-count 0
                        ;; shader program
                        :shader-filename shader-filename
                        :vs-id 0
@@ -57,58 +55,33 @@
         vertices-buffer (-> (BufferUtils/createFloatBuffer (count vertices))
                             (.put vertices)
                             (.flip))
-        ;;indices (byte-array
-        ;;         (map byte
-        ;;              [0 1 2  2 3 4]))
-        indices-count (count vertices) ;; FIXME
-        ;;indices-buffer (-> (BufferUtils/createByteBuffer indices-count)
-        ;;                   (.put indices)
-        ;;                   (.flip))
-        ;; create & bind Vertex Array Object
-        ;;vao-id (GL20/glGenVertexArrays) ;; FIXME
-        ;;_ (GL20/glBindVertexArray vao-id) ;; FIXME
-        ;; create & bind Vertex Buffer Object for vertices
+        vertices-count (count vertices) ;; FIXME
         vbo-id (GL15/glGenBuffers)
         _ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo-id)
         _ (GL15/glBufferData GL15/GL_ARRAY_BUFFER vertices-buffer GL15/GL_STATIC_DRAW)
-        ;;_ (GL20/glVertexAttribPointer 0 4 GL11/GL_FLOAT false 0 0)
-        ;;_ (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
-        ;; deselect the VAO
-        ;;_ (GL20/glBindVertexArray 0) ;; FIXME
-        ;; create & bind VBO for indices
-        ;;vboi-id (GL15/glGenBuffers)
-        ;;_ (GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER vboi-id)
-        ;;_ (GL15/glBufferData GL15/GL_ELEMENT_ARRAY_BUFFER indices-buffer GL15/GL_STATIC_DRAW)
-        ;;_ (GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER 0)
         ;;_ (println "init-buffers errors?" (GL11/glGetError))
         ]
     (dosync (ref-set globals
                      (assoc @globals
-                       ;;:vao-id vao-id
                        :vbo-id vbo-id
                        ;;:vboi-id vboi-id
-                       :indices-count indices-count)))))
+                       :vertices-count vertices-count)))))
 
 (def vs-shader
   (str "#version 120\n"
-       ;;"#version 150 core\n"
-       ;;"\n"
-       ;;"in vec4 in_Position;\n"
        "attribute vec4 in_Position;\n"
-       "\n"
        "void main(void) {\n"
        "    gl_Position = in_Position;\n"
        "}\n"
        ))
 
-(defn slurp-shadertoy
+(defn slurp-fs
   "do whatever it takes to modify shadertoy fragment shader source to
   be useable"
   [filename]
   (let [file-str (slurp filename)
         ;;file-str (.replace file-str "gl_FragColor" "o_FragColor")
         file-str (str "#version 120\n"
-                      ;;"#version 150 core\n"
                       "uniform vec3      iResolution;\n"
                       "uniform float     iGlobalTime;\n" 
                       ;;TODO "uniform float     iChannelTime[4];\n"
@@ -116,8 +89,7 @@
                       ;;TODO "uniform sampler2D iChannel[4];\n"
                       ;;TODO "uniform vec4      iDate;\n"
                       "uniform float     iOvertoneVolume;\n"
-                      ;; silly opengl shenanigans
-                      ;;"out vec4 o_FragColor;\n\n" 
+                      "\n"
                       file-str)]
     file-str))
                       
@@ -136,7 +108,7 @@
 (defn init-shaders
   []
   (let [vs-id (load-shader vs-shader GL20/GL_VERTEX_SHADER)
-        fs-shader (slurp-shadertoy (:shader-filename @globals))
+        fs-shader (slurp-fs (:shader-filename @globals))
         _ (println "Loading" (:shader-filename @globals))
         fs-id (load-shader fs-shader GL20/GL_FRAGMENT_SHADER)
         pgm-id (GL20/glCreateProgram)
@@ -177,28 +149,39 @@
 
 (defn try-reload-shader
   []
-  (let [{:keys [vs-id shader-filename]} @globals
-        fs-shader (slurp-shadertoy shader-filename)
-        fs-id (load-shader fs-shader GL20/GL_FRAGMENT_SHADER)
-        pgm-id (GL20/glCreateProgram)
-        _ (GL20/glAttachShader pgm-id vs-id)
-        _ (GL20/glAttachShader pgm-id fs-id)
-        _ (GL20/glLinkProgram pgm-id)
-        gl-link-status (GL20/glGetShaderi pgm-id GL20/GL_LINK_STATUS)
+  (let [{:keys [vs-id fs-id pgm-id shader-filename]} @globals
+        fs-shader (slurp-fs shader-filename)
+        new-fs-id (load-shader fs-shader GL20/GL_FRAGMENT_SHADER)
+        new-pgm-id (GL20/glCreateProgram)
+        _ (GL20/glAttachShader new-pgm-id vs-id)
+        _ (GL20/glAttachShader new-pgm-id new-fs-id)
+        _ (GL20/glLinkProgram new-pgm-id)
+        gl-link-status (GL20/glGetShaderi new-pgm-id GL20/GL_LINK_STATUS)
         ]
     (if (== gl-link-status GL11/GL_FALSE)
       (do
         (println "ERROR: Linking Shaders:")
-        (println (GL20/glGetProgramInfoLog pgm-id 10000))
+        (println (GL20/glGetProgramInfoLog new-pgm-id 10000))
+        (GL20/glUseProgram pgm-id)
         (dosync (ref-set globals (assoc @globals
                                    :reload-shader false))))
-      (do
-        (println "Reloading" shader-filename)
+      (let [_ (println "Reloading" shader-filename)
+            i-resolution-loc (GL20/glGetUniformLocation pgm-id "iResolution")
+            i-global-time-loc (GL20/glGetUniformLocation pgm-id "iGlobalTime")
+            i-overtone-volume-loc (GL20/glGetUniformLocation pgm-id "iOvertoneVolume")]
+        (GL20/glUseProgram new-pgm-id)
+        ;; cleanup the old program 
+        (GL20/glDetachShader pgm-id vs-id)
+        (GL20/glDetachShader pgm-id fs-id)
+        (GL20/glDeleteShader fs-id)
         (dosync (ref-set globals
                          (assoc @globals
-                           :fs-id fs-id
-                           :pgm-id pgm-id
-                           :reload-shader false)))))))
+                           :fs-id new-fs-id
+                           :pgm-id new-pgm-id
+                           :reload-shader false
+                           :i-resolution-loc i-resolution-loc
+                           :i-global-time-loc i-global-time-loc
+                           :i-overtone-volume-loc i-overtone-volume-loc)))))))
 
 (defn draw
   []
@@ -206,7 +189,8 @@
                 start-time last-time i-global-time-loc
                 i-overtone-volume-loc
                 pgm-id vbo-id
-                indices-count reload-shader]} @globals
+                vertices-count reload-shader
+                old-pgm-id old-fs-id]} @globals
                 cur-time (/ (- last-time start-time) 1000.0)
                 cur-volume (try
                              (float @(get-in voltap/v [:taps "system-vol"]))
@@ -214,33 +198,21 @@
                 ;;_ (println "cur-volume" cur-volume)
                 ]
     (if reload-shader
-      (try-reload-shader))
-    
-    (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
+      (try-reload-shader)         ; this must call glUseProgram
+      (GL20/glUseProgram pgm-id)) ; else, normal path...
 
-    (GL20/glUseProgram pgm-id)
+    (GL11/glClear (bit-or GL11/GL_COLOR_BUFFER_BIT  GL11/GL_DEPTH_BUFFER_BIT))
     ;; setup our uniform
     (GL20/glUniform3f i-resolution-loc width height 0) ;; FIXME what is 3rd iResolution param
     (GL20/glUniform1f i-global-time-loc cur-time)
     (GL20/glUniform1f i-overtone-volume-loc cur-volume)
-    ;; Bind to the VAO that has all the information about the
-    ;; vertices
-    ;;(GL20/glBindVertexArray vao-id) ;; FIXME
-    ;;(GL20/glEnableVertexAttribArray 0)
-    ;; Bind to the index VBO that has all the information about the
-    ;; order of the vertices
-    ;;(GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER vboi-id)
-    ;;
+    ;; get vertex array ready
     (GL11/glEnableClientState GL11/GL_VERTEX_ARRAY)
     (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER vbo-id)
     (GL11/glVertexPointer 4 GL11/GL_FLOAT 0 0)
     ;; Draw the vertices
-    ;;(GL11/glDrawElements GL11/GL_TRIANGLES indices-count GL11/GL_UNSIGNED_BYTE 0)
-    (GL11/glDrawArrays GL11/GL_TRIANGLES 0 indices-count)
+    (GL11/glDrawArrays GL11/GL_TRIANGLES 0 vertices-count)
     ;; Put everything back to default (deselect)
-    ;;(GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER 0)
-    ;;(GL20/glDisableVertexAttribArray 0)
-    ;;(GL20/glBindVertexArray 0) ;; FIXME
     (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
     (GL11/glDisableClientState GL11/GL_VERTEX_ARRAY)
     (GL20/glUseProgram 0)
@@ -256,33 +228,17 @@
 
 (defn destroy-gl
   []
-  (let [{:keys [pgm-id vs-id fs-id vao-id vbo-id vboi-id]} @globals]
+  (let [{:keys [pgm-id vs-id fs-id vbo-id]} @globals]
     ;; Delete the shaders
     (GL20/glUseProgram 0)
     (GL20/glDetachShader pgm-id vs-id)
     (GL20/glDetachShader pgm-id fs-id)
- 
     (GL20/glDeleteShader vs-id)
     (GL20/glDeleteShader fs-id)
     (GL20/glDeleteProgram pgm-id)
- 
-    ;; Select the VAO
-    ;;(GL20/glBindVertexArray vao-id) ;; FIXME
- 
-    ;; Disable the VBO index from the VAO attributes list
-    ;;(GL20/glDisableVertexAttribArray 0)
- 
     ;; Delete the vertex VBO
     (GL15/glBindBuffer GL15/GL_ARRAY_BUFFER 0)
     (GL15/glDeleteBuffers vbo-id)
- 
-    ;; Delete the index VBO
-    ;;(GL15/glBindBuffer GL15/GL_ELEMENT_ARRAY_BUFFER 0)
-    ;;(GL15/glDeleteBuffers vboi-id)
- 
-    ;; Delete the VAO
-    ;;(GL20/glBindVertexArray 0) ;; FIXME
-    ;;(GL20/glDeleteVertexArrays vao-id) ;; FIXME
     ))
 
 (defn run
@@ -306,13 +262,11 @@
       (while (not= :no (:active @globals))
         (Thread/sleep 100)))
     ;; start the requested shader
-    ;; FIXME -- find a more clojure-tastic way
+    ;; FIXME is there a more clojure-tastic way?
     (.start (Thread.
              (fn [] (run width height shader-filename))))))
 
-;; (start-run-thread 800 800 "shaders/simple.glsl")
-;; (start-run-thread 800 800 "shaders/quasicrystal.glsl")
-
+;; watch the shader directory & reload the current shader if it changes.
 (defn if-match-reload-shader
   [files]
   (doseq [f files]
