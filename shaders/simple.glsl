4315c50c6c935c6111132829c579e0102a9a7636
Roger Allen <rallen@gmail.com>
2013-03-13 23:50:29 -0700

    Make simple.glsl a little more complicated, but more interesting.

--- shaders/simple.glsl
+++ shaders/simple.glsl
@@ -1,10 +1,16 @@
 // The shadertoy uniform variables are available by default.
+
 // It is necessary to add the Overtone vars.
 uniform float iOvertoneVolume;
+
 void main(void) {
-  vec2 vUV = (gl_FragCoord.xy / iResolution.xy);
-  gl_FragColor = vec4(vUV.x,
-                      10.0*iOvertoneVolume,
-                      0.5*sin(5.0*iGlobalTime) + 0.5,
+  vec2 uv = (gl_FragCoord.xy / iResolution.xy);
+  // find the center and use distance from the center to vary the
+  // green component
+  vec2 uv2 = uv - 0.5;
+  float r = sqrt(uv2.x*uv2.x + uv2.y*uv2.y);
+  gl_FragColor = vec4(uv.x,
+                      20.0*iOvertoneVolume*(1-r),
+                      0.5*sin(3.0*iGlobalTime)+0.5,
                       1.0);
 }
