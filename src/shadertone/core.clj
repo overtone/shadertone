7510e9ca4b714115de9085f68fbcc1d403174eff
Roger Allen <rallen@gmail.com>
2013-04-17 07:32:16 -0700

    fix demo filename

--- src/shadertone/core.clj
+++ src/shadertone/core.clj
@@ -3,4 +3,5 @@
 ;; not quite sure what to put here...suggestions?  What should
 ;; 'lein run' do for shadertone?
 
-;; See examples/demo0.clj for the example code that was here before.
+;; See examples/00demo_intro_tour.clj for the example code that was
+;; once here.
