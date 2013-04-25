fbd5bc69672cc38d24ff94a54b5315b4760215e7
Roger Allen <rallen@gmail.com>
2013-04-24 20:59:13 -0700

    add leipzig drum machine

--- examples/02demo_disco.clj
+++ examples/02demo_disco.clj
@@ -3,13 +3,36 @@
 ;; executed at once.  It demonstrates how we can interact with
 ;; Overtone.
 (ns demo2
-  (:require [shadertone.tone :as t]))
+  (:use [overtone.live])
+  (:require [shadertone.tone :as t]
+            [leipzig.live    :as ll]
+            [leipzig.melody  :as lm]))
 
 (t/start "examples/disco.glsl"
          :width 800 :height 600
-         :textures [:iOvertoneAudio])
+         :textures [:overtone-audio])
 
-;; now go find some Overtone demos like
-;; examples/compositions/funk.clj
+;; now go find some Overtone demos like examples/compositions/funk.clj
+
+;; or try out some stuff in leipzig
+(def snare (sample (freesound-path 26903)))
+(def kick (sample (freesound-path 2086)))
+(def close-hihat (sample (freesound-path 802)))
+(def open-hihat (sample (freesound-path 26657)))
+(defmethod ll/play-note :default [{p :pitch}]
+  (case p
+      0 nil
+      1 (snare)
+      2 (kick)
+      3 (close-hihat)
+      4 (open-hihat)))
+(def melody (->> (lm/phrase [1 1 1 1 1 1 1 1]
+                            [2 0 2 0 2 0 2 2]) ;; <- play!
+                 (lm/with (lm/phrase
+                           [1 1 1 1 1 1 1 1]
+                           [0 0 1 1 3 3 4 0])) ;; <- play!
+                 (lm/where :time (lm/bpm (* 4 60)))))
+(ll/jam melody)
+(def melody nil) ;; to turn it off
 
 (t/stop) ; at some point...
