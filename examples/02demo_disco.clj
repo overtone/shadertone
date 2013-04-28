9807688b3abc792db12a4f8764e18ef7ee75b77f
Roger Allen <rallen@gmail.com>
2013-04-28 14:43:41 -0700

    Add setting :duration to leipzig drum machine.  Extend the beats to a "real" disco beat for some fun.

--- examples/02demo_disco.clj
+++ examples/02demo_disco.clj
@@ -19,20 +19,51 @@
 (def kick (sample (freesound-path 2086)))
 (def close-hihat (sample (freesound-path 802)))
 (def open-hihat (sample (freesound-path 26657)))
+(def clap (sample (freesound-path 48310)))
+(def gshake (sample (freesound-path 113625)))
+
 (defmethod ll/play-note :default [{p :pitch}]
   (case p
       0 nil
       1 (snare)
       2 (kick)
       3 (close-hihat)
-      4 (open-hihat)))
-(def melody (->> (lm/phrase [1 1 1 1 1 1 1 1]
-                            [2 0 2 0 2 0 2 0]) ;; <- play!
-                 (lm/with (lm/phrase
-                           [1 1 1 1 1 1 1 1]
-                           [0 0 1 0 3 1 4 0])) ;; <- play!
-                 (lm/where :time (lm/bpm (* 4 60)))))
-(ll/jam melody)
-(def melody nil) ;; to turn it off
+      4 (open-hihat)
+      5 (clap)
+      6 (gshake)))
+
+(def beats0     ;; 1   2   3   4   5   6   7   8
+  (->> (lm/phrase [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [2 0 1 0 2 0 1 0 2 0 1 0 2 0 1 0]) ;; <- adjust
+       (lm/with (lm/phrase
+                  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [0 0 5 0 0 0 5 0 0 0 5 0 0 0 5 0]))
+       (lm/with (lm/phrase
+                  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [3 0 3 0 3 0 3 0 3 0 3 0 3 0 3 0]))
+       (lm/with (lm/phrase
+                  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [6 6 0 6 6 6 0 6 6 6 0 6 6 6 0 6]))
+       (lm/where :time (lm/bpm (* 2 124)))
+       (lm/where :duration (lm/bpm (* 2 124)))))
+
+(ll/jam beats0)
+
+(def beats0     ;; 1   2   3   4   5   6   7   8
+  (->> (lm/phrase [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [2 0 1 1 2 0 1 0 2 0 1 1 2 0 1 0])
+       (lm/with (lm/phrase
+                  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [0 5 5 0 0 5 5 0 0 5 5 0 0 5 5 0]))
+       (lm/with (lm/phrase
+                  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [3 0 0 3 3 0 3 0 3 0 3 0 3 0 3 0]))
+       (lm/with (lm/phrase
+                  [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1]
+                  [6 6 0 6 0 6 0 6 0 6 0 6 0 6 0 6]))
+       (lm/where :time (lm/bpm (* 2 124)))
+       (lm/where :duration (lm/bpm (* 2 124)))))
+
+(def beats0 nil) ;; to turn off the melody
 
 (t/stop) ; at some point...
