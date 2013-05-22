d140f2f8152ca83999a4bd502e379d4c1eaae0f9
Roger Allen <rallen@gmail.com>
2013-05-21 21:59:07 -0700

    Translation works for a couple rudimentary testcases.  Probably doing this wrong, but at least it seems to be pretty close.  Ooops, just noticed some fails in the wave shader, so it isn't quite there...

--- src/shadertone/translate.clj
+++ src/shadertone/translate.clj
@@ -3,49 +3,74 @@
   shadertone.translate
   (:require [clojure.walk :as walk]))
 
-;; WORK IN PROGRESS -- NOTHING WORKS YET
+;; WORK IN PROGRESS -- Barely working...
 
 ;; ======================================================================
 ;; translation functions for a dialect of clojure-like s-expressions
+(declare shader-walk)
 (defn- shader-assign-str [z]
-  (let [[x y] z]
-    (println "shader-assign-str" x y)
-    (format "%s = %s;\n" x (shader-walk (list y)))))
-
+  (let [[type name value] z
+        ;;_ (println "shader-assign-str-0:" type name value)
+        type-str (if (nil? type) "" (format "%s " (str type)))
+        asn-str (format "%s%s = %s;\n" type-str name (shader-walk (list value)))]
+    ;;(println "shader-assign-str-1:" asn-str)
+    asn-str))
 (defn- shader-walk-let [x]
-  (println "shader-walk-let:" x)
-  (let [var-str  (apply str (map shader-assign-str (partition 2 (nth x 1))))
+  ;;(println "shader-walk-let-0:" x)
+  (let [var-str  (apply str (map shader-assign-str (partition 3 (nth x 1))))
         stmt-str (apply str (map #(shader-walk (list %)) (butlast (drop 2 x))))
-        ret-str  (let [v (first (shader-walk (list (last (drop 2 x)))))]
-                   (if (nil? v)
-                     ";\n"
-                     (format "return %s;\n" v)))]
+        ret-str  (let [v (shader-walk (list (last (drop 2 x))))]
+                   (if (nil? (first v))
+                     ""
+                     (format "return(%s);\n" v)))]
+    ;;(println "shader-walk-let-1:" var-str stmt-str ret-str)
     (str var-str stmt-str ret-str)))
 (defn- shader-walk-defn-args [x]
+  ;;(println "shader-walk-defn-args-0" x (empty? x))
   (if (empty? x)
     "void"
-    (identity x))) ;; FIXME
+    (apply str (interpose \, (map #(apply (partial format "%s %s") %) (partition 2 x))))))
 (defn- shader-walk-slfn [x]
- (let [fn-str (format "%s %s(%s) {\n%s}\n"
+  ;;(println "shader-walk-slfn-0:" x)
+  (let [fn-str (format "%s %s(%s) {\n%s}\n"
                       (nth x 1)
                       (nth x 2)
                       (shader-walk-defn-args (nth x 3))
                       (shader-walk (list (nth x 4))))]
+   ;;(print "shader-walk-slfn-1:" fn-str)
    fn-str))
 (defn- shader-walk-fn [x]
-  (let [fn-str (format "%s(%s);\n"
-                       (str (first x))
-                       (apply str (interpose \, (map shader-walk (rest x)))))]
-    ;;(println "shader-walk-fn" fn-str)
+  ;;(println "shader-walk-fn-0:" x)
+  (let [pre-fn (if (= (first (str (first x))) \.) "" (str (first x)))
+        post-fn (if (= (first (str (first x))) \.) (str (first x)) "")
+        fn-str (format "%s(%s)%s"
+                       pre-fn
+                       (apply str (interpose \, (map shader-walk (rest x))))
+                       post-fn)]
+    ;;(println "shader-walk-fn-1:" fn-str)
     fn-str))
+(defn- shader-walk-infix [x]
+  ;;(println "shader-walk-infix-0:" x)
+  (let [fn-str (format "(%s)"
+                       (apply str (interpose (format " %s " (str (first x)))
+                                             (map #(shader-walk (list %))
+                                                  (rest x)))))]
+    ;;(println "shader-walk-infix-1:" fn-str)
+    fn-str))
+(defn- infix-operator? [x]
+  (not (nil? (get #{ "+" "-" "*" "/"} x))))
+(defn- shader-uniform [x]
+  (format "%s;\n" (apply str (interpose \  x))))
 (defn- inner-walk
   [x]
   (do
-    (println "in:  " x);; "list?" (list? x) "vector?" (vector? x) "symbol?" (symbol? x))
+    ;;(println "in:  " x);; "list?" (list? x) "vector?" (vector? x) "symbol?" (symbol? x))
     (cond
      (list? x)     (cond
                     (= "slfn" (str (first x))) (shader-walk-slfn x)
-                    (= "let" (str (first x))) (shader-walk-let x)
+                    (= "slet" (str (first x))) (shader-walk-let x)
+                    (= "uniform" (str (first x))) (shader-uniform x)
+                    (infix-operator? (str (first x))) (shader-walk-infix x)
                     :else (shader-walk-fn x))
      (symbol? x)   (identity x)
      (float? x)    (identity x)
@@ -53,24 +78,22 @@
      :else         (shader-walk x))))
 (defn- outer-walk [x]
   (do
-    (println "out: " x)
+    ;;(println "out: " x)
     (cond
-     (list? x)     (first x)
+     (list? x)     (apply str x)
      :else         (identity x))))
 (defn- shader-walk [form]
   (walk/walk inner-walk outer-walk form))
 (defn- create-shader
   [& params]
-  (list
-   (va
-  (shader-walk (:fragment (first params)))) ;; FIXME
+  (shader-walk (first params)))
 
 ;; ======================================================================
 ;; Public API
 (defmacro defshader
-  "macro to define the fragment shader. returns shader"
-  [name & params]
-  `(def ~name (create-shader (apply hash-map (quote ~params)))))
+  "macro to define the fragment shader. returns shader as a string."
+  [name body]
+  `(def ~name (create-shader ~body)))
 
 ;; (str shader) should return the shader text ready to compile
 
@@ -79,45 +102,43 @@
 (comment
   ;; simplest possible shader
   (defshader simplest
-    :fragment
-    ((slfn void main []
-           (let [gl_FragCoord (vec4 1.0 0.5 0.5 1.0)]
-             nil))))
-  (str simplest)
+    '((slfn void main []
+            (slet [nil gl_FragCoord (vec4 1.0 0.5 0.5 1.0)]
+                  nil))))
+  (print simplest)
 
   ;; simple test
   (defshader simple
-    :varying [gl_FragCoord vec2]
-    :uniform [iResolution  vec3]
-    :fragment
-    ((slfn void main []
-           (let [uv (/ gl_FragCoord.xy iResolution.xy)
-                 gl_FragColor (vec4 uv.x uv.y 0.0 1.0)]
+    '((uniform vec3 iResolution)
+      (slfn void main []
+            (slet [vec2 uv (/ gl_FragCoord.xy iResolution.xy)
+                   nil gl_FragColor (vec4 uv.x uv.y 0.0 1.0)]
              nil))))
-
-  (str simple)
+  (print simple)
 
   ;; preliminary translation of wave.glsl
   (defshader wave
-    :varying [gl_FragCoord vec2]
-    :uniform [iResolution  vec3
-              iChannel0    sampler2D]
-    :fragment ;; return value is gl_FragColor vec4
-    ((slfn float smoothbump [center width x]
-           (let [w2 (/ width 2.0)
-                 cp (+ center w2)
-                 cm (- center w2)]
-             (* (smoothstep cm center x)
-                (- 1.0 (smoothstep center cp x)))))
-     (slfn void main []
-           (let [uv     (/ gl_FragCoord.xy iResolution.xy)
-                 uv.y   (- 1.0 uv.y)
-                 freq   (.x (texture2D iChannel0 (vec2 uv.x 0.25)))
-                 wave   (.x (texture2D iChannel0 (vec2 uv.x 0.75)))
-                 freqc  (smoothstep 0.0 (/ 1.0 iResolution.y) (+ freq uv.y -0.5))
-                 wavec  (smoothstep 0.0 (/ 4.0 iResolution.y) (+ wave uv.y -0.5))
-                 gl_FragColor (vec4 freqc wavec 0.25 1.0)]
-             nil))))
-  (str wave)
+    '((uniform vec3 iResolution)
+      (uniform sampler2D iChannel0)
+      (slfn float smoothbump
+            [float center
+             float width
+             float x]
+            (slet [float w2 (/ width 2.0)
+                   float cp (+ center w2)
+                   float cm (- center w2)]
+                  (* (smoothstep cm center x)
+                     (- 1.0 (smoothstep center cp x)))))
+      (slfn void main
+            []
+            (slet [float uv     (/ gl_FragCoord.xy iResolution.xy)
+                   nil   uv.y   (- 1.0 uv.y)
+                   float freq   (.x (texture2D iChannel0 (vec2 uv.x 0.25)))
+                   float wave   (.x (texture2D iChannel0 (vec2 uv.x 0.75)))
+                   float freqc  (smoothstep 0.0 (/ 1.0 iResolution.y) (+ freq uv.y -0.5))
+                   float wavec  (smoothstep 0.0 (/ 4.0 iResolution.y) (+ wave uv.y -0.5))
+                   nil   gl_FragColor (vec4 freqc wavec 0.25 1.0)]
+                  nil))))
+  (print wave)
 
   )
