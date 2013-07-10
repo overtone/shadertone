d24e51e363a43a0556645b544af3a8eabfeaf63c
Roger Allen <rallen@gmail.com>
2013-07-09 20:30:55 -0700

    Update to leipzig 0.6.0

--- project.clj
+++ project.clj
@@ -25,8 +25,9 @@
                  [org.lwjgl.lwjgl/lwjgl-platform "2.8.5"
                   :classifier    ~(lwjgl-classifier)
                   :native-prefix ""]
+                 ;;[overtone "0.9.0-SNAPSHOT"]
                  [overtone "0.8.1"]
                  [watchtower "0.1.1"]]
   :main ^{:skip-aot true} shadertone.core
   ;; add leipzig for use in examples
-  :profiles {:dev {:dependencies [[leipzig "0.5.0"]]}})
+  :profiles {:dev {:dependencies [[leipzig "0.6.0"]]}})
