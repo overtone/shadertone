b861ad3f890413ed9cc363d4db39c2916eaec919
Roger Allen <rallen@gmail.com>
2014-03-08 09:10:52 -0800

    Move to 0.2.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shadertone "0.2.3"
+(defproject shadertone "0.2.4-SNAPSHOT"
   :description "A clojure library designed to mix musical synthesis via Overtone and dynamic visuals a la www.shadertoy.com"
   :url "http://github.com/overtone/shadertone"
   :license {:name "MIT License"
